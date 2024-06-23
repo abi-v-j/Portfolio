8da53a8c177df59dda39a3ed6fb3653cac81cf11
abi-v-j <abijoy611@gmail.com>
2024-06-23 06:09:50 -0700

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Router.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
